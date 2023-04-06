b5b15363fe0c6afac77172c8f00664b97c50c195
Javkhlant Altankhuyag <enroseking95@gmail.com>
2023-04-06 18:51:02 +0800

    Finished initializing backend project

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,27 @@
-import express, { Express } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import createHttpError, { isHttpError } from 'http-errors';
 
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Routes
+
+app.use((req, res, next) => {
+  next(createHttpError(404, 'Endpoint not found'));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  let errorMessage = 'An unknown error occurred';
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
+});
 
 export default app;
