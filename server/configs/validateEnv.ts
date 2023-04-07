9ce39e928b79b896b76c4142e663a17a42ab567f
Javkhlant Altankhuyag <enroseking95@gmail.com>
2023-04-07 17:48:25 +0800

    Added cloudinary keys to .env

--- server/configs/validateEnv.ts
+++ server/configs/validateEnv.ts
@@ -5,4 +5,7 @@ import { port, str } from 'envalid/dist/validators';
 export default cleanEnv(process.env, {
   MONGO_CONNECTION_STRING: str(),
   PORT: port(),
+  CLOUDINARY_CLOUD_NAME: str(),
+  CLOUDINARY_API_KEY: str(),
+  CLOUDINARY_API_SECRET: str(),
 });
