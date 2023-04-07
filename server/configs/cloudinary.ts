de06507c6fb710c75cd31d092b0a2569f91804f4
Binderiya <binderiyatsogthers@gmail.com>
2023-04-07 16:12:53 +0800

    install cloudinary and create multer and cloudinary configs

---
+++ server/configs/cloudinary.ts
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: 'dvlgyc6gs',
+  api_key: '581945756271636',
+  api_secret: 'ejnrRYW2FckG2tQs0YIEbeuCiyM',
+});
+
+export default cloudinary;
