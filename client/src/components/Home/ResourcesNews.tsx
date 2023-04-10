27355035d5966e3ec780cc7a3a3845f2b5e373f3
Ganbold <u.boldoo@gmail.com>
2023-04-10 15:55:05 +0800

    edit

--- client/src/components/Home/ResourcesNews.tsx
+++ client/src/components/Home/ResourcesNews.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import Image from "next/image";
 import bimage from "../../assets/backimg.svg";
+import ArrowButton from "../ArrowButton";
 
 export const ResourcesNews: FC = () => (
   <div className="container">
@@ -11,26 +12,12 @@ export const ResourcesNews: FC = () => (
           Lorem ipsum dolor sit amet, consectetur
         </p>
       </div>
-      <button className="w-[176px] h-[50px] bg-color-1 rounded-md">
+      <ArrowButton className="bg-color-1/[.07] text-color-1">
         Browse Blog
-      </button>
+      </ArrowButton>
     </div>
     <div className="grid grid-cols-3 gap-[15px] pb-[112px]">
-      <div className="">
-        <Image
-          src={bimage}
-          alt=""
-          className="rounded-xl pb-[5px]"
-          width={435}
-          height={354}
-        />
-        <p className="text-color-1 text-sm-medium pb-[10px]">EDUCATION</p>
-        <h1 className="text-[#242239] text-2xl-medium pb-[10px]">
-          Eco-Education in Our Lives: We Can Change the Future
-        </h1>
-        <p className="text-text text-md-regular">December 16, 2022</p>
-      </div>
-      <div className="">
+      <div className="flex flex-col gap-[10px]">
         <Image
           src={bimage}
           alt=""
@@ -41,14 +28,28 @@ export const ResourcesNews: FC = () => (
         <p className="text-color-1 text-sm-medium pb-[10px] uppercase">
           Education
         </p>
-        <h1 className="text-[#242239] text-2xl-medium pb-[10px]">
+        <h1 className="text-[#242239] text-2xl-medium">
           Eco-Education in Our Lives: We Can Change the Future
         </h1>
         <p className="text-text text-md-regular">December 16, 2022</p>
       </div>
+      <div className="flex flex-col gap-[10px]">
+        <Image
+          src={bimage}
+          alt=""
+          className="rounded-xl pb-[5px]"
+          width={435}
+          height={354}
+        />
+        <p className="text-color-1 text-sm-medium uppercase">Design</p>
+        <h1 className="text-[#242239] text-2xl-medium">
+          How to design a simple, yet unique and memorable brand identity
+        </h1>
+        <p className="text-text text-md-regular">December 16, 2022</p>
+      </div>
 
-      <div className="">
-        <div className="flex items-center justify-between pb-[13px]">
+      <div className="flex flex-col justify-between">
+        <div className="flex items-center justify-between">
           <Image
             src={bimage}
             alt=""
@@ -66,7 +67,7 @@ export const ResourcesNews: FC = () => (
             <p className="text-text text-md-regular">December 16, 2022</p>
           </div>
         </div>
-        <div className="pb-[13px]">
+        <div className="flex items-center justify-between">
           <Image
             src={bimage}
             alt=""
@@ -76,7 +77,7 @@ export const ResourcesNews: FC = () => (
           />
           <div className="pl-[10px]">
             <h1 className="text-color-1 text-xs-medium uppercase pb-[10px]">
-              Courses
+              Design
             </h1>
             <h3 className="text-head text-lg-medium pb-[10px]">
               Medical Chemistry: The Molecular Basis
@@ -84,7 +85,7 @@ export const ResourcesNews: FC = () => (
             <p className="text-text text-md-regular">December 16, 2022</p>
           </div>
         </div>
-        <div className="pb-[13px]">
+        <div className="flex items-center justify-between">
           <Image
             src={bimage}
             alt=""
