60984400fdc7336e044c364615db8852ab828998
Binderiya <binderiyatsogthers@gmail.com>
2023-04-11 18:04:25 +0800

    Instructors page is done for me(!pagination)

--- client/src/pages/instructors/index.tsx
+++ client/src/pages/instructors/index.tsx
@@ -1,5 +1,160 @@
-const InstructorsPage = () => {
-  return <div>index</div>;
+import { ICourseCategory } from '@/interfaces/courses';
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
+import { FC, useEffect, useState } from 'react';
+import { InstructorCard } from '@/components/Home/subComponents/InstructorCard';
+import Breadcrumbs from '@/components/Breadcrumbs';
+import { title } from 'process';
+import { BsChevronDown, BsSearch } from 'react-icons/bs';
+
+interface CoursesPageProps {
+  categories: ICourseCategory[];
+}
+
+export const getServerSideProps: GetServerSideProps<
+  CoursesPageProps
+> = async () => {
+  const res = await axios.get('http://localhost:5000/api/courses/categories');
+  return {
+    props: {
+      categories: res.data.body,
+    },
+  };
+};
+
+const InstructorsPage: FC<CoursesPageProps> = () => {
+  const [dropCategory, setDropCategory] = useState(false);
+  const [dropSort, setDropSort] = useState(false);
+
+  const dropCategoryHandler = () => {
+    setDropCategory(!dropCategory);
+  };
+
+  const dropSortHandler = () => {
+    setDropSort(!dropSort);
+  };
+
+  return (
+    <>
+      <Breadcrumbs
+        breadcrumbItems={[{ title: 'Багш, сургагч нар', link: '/instructors' }]}
+      />
+      <div className="container mb-[150px]">
+        <div className="text-center">
+          <h1 className="font-[700] text-[40px] leading-[47px] text-head mb-1">
+            Instructors
+          </h1>
+          <p className="text-lg-regular text-text mb-[90px]">
+            We’re on a mission to deliver engaging, curated courses at a
+            reasonable price.
+          </p>
+        </div>
+        <div className="flex justify-between items-center mb-[30px]">
+          <p className="text-text text-sm-regular">
+            Showing <span className="text-head text-sm-medium">250</span> total
+            results
+          </p>
+          <div className="flex justify-between gap-[23px]">
+            <div className="flex items-center gap-[20px] text-icon bg-bg-4 rounded-lg px-[18px] w-[340px]">
+              <BsSearch />
+              <input
+                type="text"
+                className=" placeholder:text-text text-sm-regular bg-inherit w-full h-full"
+                placeholder="Search Instructors"
+              />
+            </div>
+
+            <div className="relative">
+              <button
+                onClick={dropCategoryHandler}
+                className="bg-bg-4 rounded-lg py-4 px-[15px] flex items-center gap-[46px] text-text text-sm-regular"
+              >
+                Category
+                <BsChevronDown
+                  className={`transition-all ${
+                    dropCategory ? 'rotate-[-180deg]' : 'rotate-0'
+                  }`}
+                />
+              </button>
+              <div
+                className={`${
+                  dropCategory ? 'block' : 'hidden'
+                } absolute top-[60px] z-[10] bg-bg-4 rounded-lg py-[22px] pl-[30px] pr-[50px]`}
+              >
+                <ul className="flex flex-col font-[400] text-[15px] leading-[35px] text-head ">
+                  <li className="hover:text-color-1 whitespace-nowrap cursor-pointer hover:underline">
+                    Web Designer
+                  </li>
+                  <li className="hover:text-color-1 whitespace-nowrap cursor-pointer hover:underline">
+                    Marketing Coordinator
+                  </li>
+                  <li className="hover:text-color-1 whitespace-nowrap cursor-pointer hover:underline">
+                    Medical Assistant
+                  </li>
+                  <li className="hover:text-color-1 whitespace-nowrap cursor-pointer hover:underline">
+                    Dog Trainer
+                  </li>
+                  <li className="hover:text-color-1 whitespace-nowrap cursor-pointer hover:underline">
+                    Chief
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="relative">
+              <button
+                onClick={dropSortHandler}
+                className="bg-bg-4 rounded-lg py-4 px-[15px] flex items-center gap-[46px] text-text text-sm-regular"
+              >
+                Sort by: Default
+                <BsChevronDown
+                  className={`transition-all ${
+                    dropSort ? 'rotate-[-180deg]' : 'rotate-0'
+                  }`}
+                />
+              </button>
+              <div
+                className={`${
+                  dropSort ? 'block' : 'hidden'
+                } absolute top-[60px] z-[10] bg-bg-4 rounded-lg py-[22px] pl-[30px] pr-[50px]`}
+              >
+                <ul className="flex flex-col font-[400] text-[15px] leading-[35px] text-head ">
+                  <li className="hover:text-color-1 whitespace-nowrap cursor-pointer hover:underline ">
+                    Popular
+                  </li>
+                  <li className="hover:text-color-1 whitespace-nowrap cursor-pointer hover:underline">
+                    Newest
+                  </li>
+                  <li className="hover:text-color-1 whitespace-nowrap cursor-pointer hover:underline">
+                    A - Z
+                  </li>
+                  <li className="hover:text-color-1 whitespace-nowrap cursor-pointer hover:underline">
+                    Z - A
+                  </li>
+                  <li className="hover:text-color-1 whitespace-nowrap cursor-pointer hover:underline">
+                    Chief
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-4 gap-[30px] mb-[60px]">
+          <InstructorCard />
+          <InstructorCard />
+          <InstructorCard />
+          <InstructorCard />
+          <InstructorCard />
+          <InstructorCard />
+          <InstructorCard />
+          <InstructorCard />
+          <InstructorCard />
+          <InstructorCard />
+          <InstructorCard />
+          <InstructorCard />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default InstructorsPage;
