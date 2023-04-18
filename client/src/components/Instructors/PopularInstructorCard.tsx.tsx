cb28f7465609660ab146e8df2d8f10ab0b71cac7
binderiyats <binderiyatsogthers@gmail.com>
2023-04-18 16:00:47 +0800

    working on category filter

---
+++ client/src/components/Instructors/PopularInstructorCard.tsx.tsx
@@ -0,0 +1,70 @@
+import { FC } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import placeholder from "../../assets/placeholder.png";
+import RatingStar from "../global/RatingStar";
+
+import { AiOutlineUser } from "react-icons/ai";
+import { BiRightArrow } from "react-icons/bi";
+import { ImFacebook, ImTwitter, ImLinkedin2 } from "react-icons/im";
+import { BsInstagram } from "react-icons/bs";
+const PopularInstructorCard: FC = () => (
+  <div className="flex flex-col justify-center items-center">
+    <Link
+      href="/"
+      className="rounded-full overflow-hidden w-[180px] h-[180px] mb-[15px] group relative"
+    >
+      <Image src={placeholder} alt="" className="w-full h-full object-cover" />
+      <div className="absolute w-full h-full top-0 right-0 left-0 bottom-0 bg-head/0 opacity-0 group-hover:bg-head/50 group-hover:opacity-100 duration-300 flex justify-center items-center">
+        <Link
+          className=" p-2 rounded-full hover:bg-white/10 hover:text-white duration-300 text-sm"
+          href="/"
+        >
+          <ImFacebook />
+        </Link>
+        <Link
+          className=" p-2 rounded-full hover:bg-white/10 hover:text-white duration-300 text-sm"
+          href="/"
+        >
+          <ImTwitter />
+        </Link>
+        <Link
+          className=" p-2 rounded-full hover:bg-white/10 hover:text-white duration-300 text-sm"
+          href="/"
+        >
+          <BsInstagram />
+        </Link>
+        <Link
+          className=" p-2 rounded-full hover:bg-white/10 hover:text-white duration-300 text-sm"
+          href="/"
+        >
+          <ImLinkedin2 />
+        </Link>
+      </div>
+    </Link>
+    <span className="flex justify-center mb-[6px] gap-[5px]">
+      <RatingStar className="fill-[#E59819]" />
+      <p className="text-[#E59819] text-sm-medium">4.5</p>
+    </span>
+    <Link
+      href="/instructors/instructor"
+      className="block text-head text-lg-medium hover:text-head/80 duration-300 mb-[5px] text-center"
+    >
+      Floyd Miles
+    </Link>
+    <p className="text-text mb-[11px] text-center">President of Sales</p>
+    <div className="flex gap-[15px]">
+      <span className="flex items-center  gap-2 text-md text-icon">
+        <AiOutlineUser />
+        <span className="text-text text-sm-regular">365 Students</span>
+      </span>
+      <span className="flex items-center  gap-2 text-md text-icon">
+        <BiRightArrow />
+        <span className="text-text text-sm-regular">15 Courses</span>
+      </span>
+    </div>
+  </div>
+);
+
+export default PopularInstructorCard;
--- client/src/pages/instructors/index.tsx
+++ client/src/pages/instructors/index.tsx
@@ -1,18 +1,20 @@
-import axios from 'axios';
-import { GetServerSideProps } from 'next';
-import { FC, useState } from 'react';
-import { BsChevronDown, BsSearch } from 'react-icons/bs';
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import { FC, useState } from "react";
+import { BsChevronDown, BsSearch } from "react-icons/bs";
 
-import { ICourseCategory } from '@/interfaces/courses';
-import Breadcrumbs from '@/components/global/Breadcrumbs';
-import InstructorCard from '@/components/Instructors/InstructorCard';
+import { ICourseCategory } from "@/interfaces/courses";
+import Breadcrumbs from "@/components/global/Breadcrumbs";
+import InstructorCard from "@/components/Instructors/InstructorCard";
 
 interface CoursesPageProps {
   categories: ICourseCategory[];
 }
 
-export const getServerSideProps: GetServerSideProps<CoursesPageProps> = async () => {
-  const res = await axios.get('http://localhost:5000/api/courses/categories');
+export const getServerSideProps: GetServerSideProps<
+  CoursesPageProps
+> = async () => {
+  const res = await axios.get("http://localhost:5000/api/courses/categories");
   return {
     props: {
       categories: res.data.body,
@@ -36,17 +38,23 @@ const InstructorsPage: FC<CoursesPageProps> = () => {
 
   return (
     <>
-      <Breadcrumbs breadcrumbItems={[{ title: 'Багш, сургагч нар', link: '/instructors' }]} />
+      <Breadcrumbs
+        breadcrumbItems={[{ title: "Багш, сургагч нар", link: "/instructors" }]}
+      />
       <div className="container mb-[150px]">
         <div className="text-center">
-          <h1 className="font-[700] text-[40px] leading-[47px] text-head mb-1">Instructors</h1>
+          <h1 className="font-[700] text-[40px] leading-[47px] text-head mb-1">
+            Instructors
+          </h1>
           <p className="text-lg-regular text-text mb-[90px]">
-            We’re on a mission to deliver engaging, curated courses at a reasonable price.
+            We’re on a mission to deliver engaging, curated courses at a
+            reasonable price.
           </p>
         </div>
         <div className="flex justify-between items-center mb-[30px]">
           <p className="text-text text-sm-regular">
-            Showing <span className="text-head text-sm-medium">250</span> total results
+            Showing <span className="text-head text-sm-medium">250</span> total
+            results
           </p>
           <div className="flex justify-between gap-[23px]">
             <div className="flex items-center gap-[20px] text-icon bg-bg-4 rounded-lg px-[18px] w-[340px] focus-within:ring-2 focus-within:ring-color-1 duration-300">
@@ -71,12 +79,14 @@ const InstructorsPage: FC<CoursesPageProps> = () => {
               >
                 Category
                 <BsChevronDown
-                  className={`duration-300 ${dropCategory ? 'rotate-[-180deg]' : 'rotate-0'}`}
+                  className={`duration-300 ${
+                    dropCategory ? "rotate-[-180deg]" : "rotate-0"
+                  }`}
                 />
               </button>
               <div
                 className={`${
-                  dropCategory ? 'opacity-100' : 'opacity-0 pointer-events-none'
+                  dropCategory ? "opacity-100" : "opacity-0 pointer-events-none"
                 } absolute top-[60px] z-[10] bg-bg-4 rounded-lg py-[22px] pl-[30px] pr-[50px] duration-300 shadow-lg`}
               >
                 <ul className="flex flex-col font-[400] text-[15px] leading-[35px] text-head ">
@@ -105,12 +115,14 @@ const InstructorsPage: FC<CoursesPageProps> = () => {
               >
                 Sort by: Default
                 <BsChevronDown
-                  className={`duration-300 ${dropSort ? 'rotate-[-180deg]' : 'rotate-0'}`}
+                  className={`duration-300 ${
+                    dropSort ? "rotate-[-180deg]" : "rotate-0"
+                  }`}
                 />
               </button>
               <div
                 className={`${
-                  dropSort ? 'opacity-100' : 'opacity-0 pointer-events-none'
+                  dropSort ? "opacity-100" : "opacity-0 pointer-events-none"
                 } absolute top-[60px] z-[10] bg-bg-4 rounded-lg py-[22px] pl-[30px] pr-[50px] duration-300 shadow-lg`}
               >
                 <ul className="flex flex-col font-[400] text-[15px] leading-[35px] text-head ">
