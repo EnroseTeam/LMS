4a034c233c7e13ae835ea210156df39b2830ce67
Javkhlant Altankhuyag <enroseking95@gmail.com>
2023-04-18 21:20:14 +0800

    Added HeaderAlternate and FooterAlternate for single lesson page

---
+++ client/src/pages/lessons/[id].tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+import FooterAlternate from '@/components/Lessons/FooterAlternate';
+import HeaderAlternate from '@/components/Lessons/HeaderAlternate';
+
+const SingleLessonPage: FC = () => (
+  <>
+    <HeaderAlternate />
+    <FooterAlternate />
+  </>
+);
+
+export default SingleLessonPage;
