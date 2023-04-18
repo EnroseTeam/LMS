5590ddf2ed41346993b11671d8ebfcac508e659a
binderiyats <binderiyatsogthers@gmail.com>
2023-04-18 22:28:53 +0800

    Completed RadioButtonFilter component in the global component, used in courses page.

--- client/src/components/global/RadioButtonFilter.tsx
+++ client/src/components/global/RadioButtonFilter.tsx
@@ -1,14 +1,21 @@
-import { ICourseCategory } from "@/interfaces/courses";
 import { FC, useState } from "react";
 
 import { BsChevronDown } from "react-icons/bs";
 
-interface CheckBoxFilterProps {
-  title: string;
-  items?: ICourseCategory[];
+interface Item {
+  content?: JSX.Element | string;
+  count?: number;
 }
 
-const CheckBoxFilter: FC<CheckBoxFilterProps> = ({ title, items = [] }) => {
+interface RadioButtonFilterProps {
+  title: string;
+  items?: Item[];
+}
+
+const RadioButtonFilter: FC<RadioButtonFilterProps> = ({
+  title,
+  items = [],
+}) => {
   const [show, setShow] = useState(true);
   const [displayItems, setDisplayItems] = useState(items.slice(0, 5));
 
@@ -16,12 +23,12 @@ const CheckBoxFilter: FC<CheckBoxFilterProps> = ({ title, items = [] }) => {
     <div>
       <button
         onClick={(): void => setShow(!show)}
-        className="text-head font-medium leading-[23px] text-[20px] flex justify-between w-full items-center"
+        className="flex w-full justify-between items-center text-head font-medium leading-[23px] text-[20px]"
       >
         {title}
         <BsChevronDown
-          className={`duration-300 ${show ? "rotate-[-180deg]" : "rotate-0"} `}
           size={15}
+          className={`${show ? "rotate-[-180deg]" : "rotate-0"} duration-300`}
         />
       </button>
       <div
@@ -29,24 +36,28 @@ const CheckBoxFilter: FC<CheckBoxFilterProps> = ({ title, items = [] }) => {
           show
             ? "max-h-[1000px] opacity-100 py-[30px]"
             : "max-h-0 opacity-0 pointer-events-none"
-        } z-[10] duration-150 border-b`}
+        } z-10 border-b duration-150`}
       >
-        {displayItems.map((item) => (
+        {displayItems.map((item, index) => (
           <div
-            key={item._id}
-            className="w-full flex items-center justify-between text-color-2 py-[5px] text-sm-regular"
+            key={`radio-button-${index}`}
+            className="text-color-2 py-[5px] text-sm-regular w-full flex justify-between items-center"
           >
             <div className="flex items-center gap-[15px]">
               <input
-                type="checkbox"
-                id={item.name}
-                className="w-[15px] h-[15px] border-2 border-2-icon rounded-none text-black"
+                type="radio"
+                id={`button-${index}`}
+                className="w-[15px] h-[15px] border-2 border-2-icon text-black"
+                name="default-radio"
               />
-              <label className="w-[20ch] select-none" htmlFor={item.name}>
-                {item.name}
+              <label
+                htmlFor={`button-${index}`}
+                className="select-none w-[20ch]"
+              >
+                {item.content}
               </label>
             </div>
-            <div className="text-text select-none">({item.courseCount})</div>
+            <div className="text-text select-none">({item.count})</div>
           </div>
         ))}
         {items.length > 5 && (
@@ -65,5 +76,4 @@ const CheckBoxFilter: FC<CheckBoxFilterProps> = ({ title, items = [] }) => {
     </div>
   );
 };
-
-export default CheckBoxFilter;
+export default RadioButtonFilter;
