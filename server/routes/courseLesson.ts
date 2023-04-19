7171cd02c3eb95ba75c0eafa00a16e95e5811003
Javkhlant <enrosek95@gmail.com>
2023-04-19 17:18:49 +0800

    Added single lesson page:

--- server/routes/courseLesson.ts
+++ server/routes/courseLesson.ts
@@ -1,21 +1,23 @@
-import express from 'express';
+import express from "express";
 import {
   createCourseLesson,
   deleteCourseLesson,
   getCourseLessons,
+  getLessonIds,
   getSingleCourseLesson,
   updateCourseLesson,
-} from '../controllers/courseLesson';
+} from "../controllers/courseLesson";
 
 const courseLessonRouter = express.Router();
 
-courseLessonRouter.get('/', getCourseLessons);
-courseLessonRouter.get('/:id', getSingleCourseLesson);
+courseLessonRouter.get("/ids", getLessonIds);
+courseLessonRouter.get("/", getCourseLessons);
+courseLessonRouter.get("/:id", getSingleCourseLesson);
 
-courseLessonRouter.post('/', createCourseLesson);
+courseLessonRouter.post("/", createCourseLesson);
 
-courseLessonRouter.patch('/:id', updateCourseLesson);
+courseLessonRouter.patch("/:id", updateCourseLesson);
 
-courseLessonRouter.delete('/:id', deleteCourseLesson);
+courseLessonRouter.delete("/:id", deleteCourseLesson);
 
 export default courseLessonRouter;
