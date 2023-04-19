5fd228934b0505ecf5979e0b6957992f05bad565
Ganbold <u.boldoo@gmail.com>
2023-04-19 16:14:03 +0800

    navbar and sidebar

--- admin/src/pages/_app.tsx
+++ admin/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/global/Navbar";
+import Sidebar from "@/components/global/Sidebar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -6,7 +7,10 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <Navbar />
-      <Component {...pageProps} />
+      <div className="flex">
+        <Sidebar />
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
