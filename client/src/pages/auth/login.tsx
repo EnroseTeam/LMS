80a97fda1650c6ae04fbc631f2fc888e1bec16c0
Ganbold <u.boldoo@gmail.com>
2023-04-20 17:55:44 +0800

    Login Page

--- client/src/pages/auth/login.tsx
+++ client/src/pages/auth/login.tsx
@@ -1,55 +1,56 @@
-import Link from "next/link";
+import LoginForm from "@/components/auth/LoginForm";
+import LoginNavbar from "@/components/auth/LoginNavbar";
+import Image from "next/image";
 import { FC } from "react";
-import { FaFacebookF, FaGoogle } from "react-icons/fa";
+import bgPic from "../../assets/bg.png";
+import bgPic1 from "../../assets/Group9.png";
+import bgPic2 from "../../assets/Group10.png";
+import bgPic3 from "../../assets/Group11.png";
+import logoImg from "../../assets/logo-main.svg";
 
 const Login: FC = () => (
-  <div className="grid grid-cols-3">
-    <div className=" bg-head h-screen w-screen gird grid-cols-1">1</div>
-    <div className=" items-center justify-center bg-bg-5 h-screen w-screen grid grid-cols-1 ">
-      <div className="flex flex-col items-center justify-center bg-white w-[520px] h-[603px] ">
-        <div className=" w-[470px] h-[553px]">
-          <h1 className="text-head text-3xl-bold pt-[50px] pb-[9px]">Login</h1>
-          <p>Don't have an account yet?</p>
-          <Link href="/" className="text-color-1">
-            Sign up for free
-          </Link>
-          <p className="text-head text-base-medium pt-[30px] pb-[9px]">
-            Username Or Email
-          </p>
-          <input
-            type="text"
-            placeholder="Ali"
-            className="border border-spacing-1 w-[420px] h-[55px] pt-[9px] rounded-xl"
-          />
-          <p className="text-head text-base-medium pt-[30px] pb-[9px]">
-            Password
-          </p>
-          <input
-            type="text"
-            placeholder="********************** "
-            className="border border-spacing-1 w-[420px] h-[55px] pt-[9px] rounded-xl"
-          />
-          <p>Username Or Email</p>
-          <input type="text" />
-          <button className="bg-color-6 w-[420px] h-[60px] rounded-lg text-head text-base-medium">
-            Login
-          </button>
-          <p>Or sign in using</p>
-          <div className="flex gap-5">
-            <button className="flex flex-row items-center justify-center border-2 rounded-md border-[#1967D2] w-[200px] h-[45px] text-head">
-              <p className="w-[2px] h-[15.6px] pr-[9px]">
-                <FaFacebookF />
-              </p>
-              <p className="pl-[9px]">Log In via Facebook</p>
-            </button>
-            <button className="flex flex-row items-center justify-center border-2 rounded-md border-[#1967D2] w-[200px] h-[45px] text-head">
-              <p className="w-[2px] h-[15.6px] pr-[9px]">
-                <FaGoogle />
-              </p>
-              <p className="pl-[9px]">Log In via Google+</p>
-            </button>
-          </div>
-        </div>
+  <div className="grid grid-cols-3 h-screen">
+    <div className="col-span-1 flex flex-col items-center justify-center bg-head h-full relative">
+      <div className="top-[33px] left-[60px] absolute">
+        <Image src={logoImg} width={134} height={50} alt="zurag" />
+      </div>
+      <div>
+        <Image
+          src={bgPic1}
+          width={86}
+          height={86}
+          alt="zurag"
+          className="absolute top-[430px] left-[150px] z-10"
+        />
+        <Image
+          src={bgPic2}
+          width={86}
+          height={86}
+          alt="zurag"
+          className="absolute top-[430px] left-[450px] z-10"
+        />
+        <Image
+          src={bgPic3}
+          width={86}
+          height={86}
+          alt="zurag"
+          className="absolute top-[700px] left-[450px] z-10"
+        />
+        <Image
+          src={bgPic}
+          width={527}
+          height={590}
+          alt="zurag"
+          className="relative"
+        />
+      </div>
+    </div>
+    <div className="col-span-2 bg-bg-5 h-full w-full grid place-items-center relative">
+      <div className="top-[33px] right-[60px] absolute">
+        <LoginNavbar />
+      </div>
+      <div className="py-[178px] px-[338px] ">
+        <LoginForm />
       </div>
     </div>
   </div>
