37bb3d7ea4df53495894e1b1a77ae1c282702e77
Javkhlant <enrosek95@gmail.com>
2023-04-20 16:40:53 +0800

    Added category filter to popular courses on homepage

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,27 +1,28 @@
-import axios from 'axios';
-import { GetServerSideProps } from 'next';
-import Head from 'next/head';
-import { FC } from 'react';
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { FC } from "react";
 
-import { ICourse, ICourseCategory } from '@/interfaces/courses';
-import ResourcesNewsSection from '@/components/Home/ResourcesNewsSection';
-import AdvantageSection from '@/components/Home/AdvantageSection';
-import BestInstructorSection from '@/components/Home/BestInstructorSection';
-import HeroSection from '@/components/Home/HeroSection';
-import PartnerSection from '@/components/Home/PartnerSection';
-import PopularCoursesSection from '@/components/Home/PopularCoursesSection';
-import TopCategoriesSection from '@/components/Home/TopCategoriesSection';
-import UsersCommentSection from '@/components/Home/UsersCommentSection';
+import { ICourse, ICourseCategory } from "@/interfaces/courses";
+import ResourcesNewsSection from "@/components/Home/ResourcesNewsSection";
+import AdvantageSection from "@/components/Home/AdvantageSection";
+import BestInstructorSection from "@/components/Home/BestInstructorSection";
+import HeroSection from "@/components/Home/HeroSection";
+import PartnerSection from "@/components/Home/PartnerSection";
+import PopularCoursesSection from "@/components/Home/PopularCoursesSection";
+import TopCategoriesSection from "@/components/Home/TopCategoriesSection";
+import UsersCommentSection from "@/components/Home/UsersCommentSection";
 
 interface HomeProps {
   categories: ICourseCategory[];
   courses: ICourse[];
 }
 
-export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+export const getServerSideProps: GetServerSideProps<HomeProps> = async ({ query }) => {
+  const { category = "" } = query;
   const [categoryRes, coursesRes] = await axios.all([
-    axios.get('http://localhost:5000/api/courses/categories'),
-    axios.get('http://localhost:5000/api/courses/'),
+    axios.get("http://localhost:5000/api/courses/categories"),
+    axios.get(`http://localhost:5000/api/courses?category=${category}`),
   ]);
 
   return {
@@ -40,7 +41,7 @@ const Home: FC<HomeProps> = ({ categories, courses }) => (
     <HeroSection />
     <PartnerSection />
     <TopCategoriesSection categories={categories} />
-    <PopularCoursesSection courses={courses} />
+    <PopularCoursesSection courses={courses} categories={categories} />
     <UsersCommentSection />
     <BestInstructorSection />
     <AdvantageSection />
