c086372d82ce46471f55924f9e8abc5a956ec9af
Ganbold02 <115781781+Ganbold02@users.noreply.github.com>
2023-04-22 18:24:12 +0800

    DropdownLarge (#150)
    Co-authored-by: Javkhlant <110245204+EnRoseK@users.noreply.github.com>

--- client/src/components/global/DropdownLarge.tsx
+++ client/src/components/global/DropdownLarge.tsx
@@ -1,100 +1,50 @@
 import Link from "next/link";
 import React, { FC } from "react";
 
-const DropdownLarge: FC = () => {
-  const links = [
-    {
-      name: "Explore",
-      submenu: true,
-      sublinks: [
-        {
-          Head: "Courses",
-          sublinks: [
-            { name: "Menu1", link: "/" },
-            { name: "Menu2", link: "/" },
-            { name: "Menu3", link: "/" },
-            { name: "Menu4", link: "/" },
-            { name: "Menu5", link: "/" },
-          ],
-        },
-        {
-          Head: "Courses2",
-          sublinks: [
-            { name: "Menu1", link: "/" },
-            { name: "Menu2", link: "/" },
-            { name: "Menu3", link: "/" },
-            { name: "Menu4", link: "/" },
-            { name: "Menu5", link: "/" },
-          ],
-        },
-        {
-          Head: "Courses2",
-          sublinks: [
-            { name: "Menu1", link: "/" },
-            { name: "Menu2", link: "/" },
-            { name: "Menu3", link: "/" },
-            { name: "Menu4", link: "/" },
-            { name: "Menu5", link: "/" },
-          ],
-        },
-        {
-          Head: "Courses2",
-          sublinks: [
-            { name: "Menu1", link: "/" },
-            { name: "Menu2", link: "/" },
-            { name: "Menu3", link: "/" },
-            { name: "Menu4", link: "/" },
-            { name: "Menu5", link: "/" },
-          ],
-        },
-      ],
-    },
-  ];
+interface SubLinks {
+  name: string;
+  link: string;
+}
 
-  return (
-    <>
-      <div className="flex items-center gap-10 text-md-regular cursor-pointer ">
-        {links.map((link, index) => (
-          <div key={`dropdown-${index}`}>
-            <div className=" px-3 text-left group">
-              <h1 className="hover:text-color-6">{link.name}</h1>
-              {link.submenu && (
-                <div>
-                  <div className="absolute top-15 hidden group-hover:block hover:block">
-                    <div className="py-3">
-                      <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45" />
-                    </div>
-                    <div className="w-[800px] h-[430px] bg-white text-head p-3.5 grid grid-cols-4 gap-5">
-                      {link.sublinks.map((mysublinks) => (
-                        <div key={`dropdown-${index}`}>
-                          <h1 className="hover:text-white/70 duration-300">
-                            {mysublinks.Head}
-                          </h1>
-                          {mysublinks.sublinks.map((slink, index) => (
-                            <li
-                              className="hover:text-white/70 duration-300"
-                              key={`mysublinks-${index}`}
-                            >
-                              <Link
-                                href={slink.link}
-                                className="hover:text-color-6"
-                              >
-                                {slink.name}
-                              </Link>
-                            </li>
-                          ))}
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              )}
+interface DropDownLargeItem {
+  Head: string;
+  sublinks: SubLinks[];
+}
+
+interface DropdownLargeProps {
+  items: DropDownLargeItem[];
+}
+
+const DropdownLarge: FC<DropdownLargeProps> = ({ items }) => (
+  <>
+    <div>
+      <div className="absolute top-15 hidden group-hover:block hover:block w-[1390px] left-[50%] -translate-x-[50%] rounded-lg">
+        {/* <div className="py-3">
+          <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45" />
+        </div> */}
+        <div className="bg-white/95 text-head p-3.5 grid grid-cols-5 gap-10 mt-10">
+          {items.map((mysublinks, index) => (
+            <div key={`dropdown-${index}`}>
+              <h1 className="hover:text-head duration-300 text-left pb-5 uppercase">
+                {mysublinks.Head}
+              </h1>
+              {mysublinks.sublinks.map((slink, index) => (
+                <li
+                  className="hover:text-head duration-300 text-left pb-3"
+                  key={`mysublinks-${index}`}
+                >
+                  <Link href={slink.link} className="hover:text-head">
+                    {slink.name}
+                  </Link>
+                </li>
+              ))}
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </>
-  );
-};
+    </div>
+  </>
+);
+
 
 export default DropdownLarge;
--- client/src/components/global/Header.tsx
+++ client/src/components/global/Header.tsx
@@ -1,86 +1,84 @@
-import { FC, useState } from "react";
+
+import { FC } from "react";
+
 import Image from "next/image";
 import Link from "next/link";
 
 import mainLogo from "@/assets/logo-main.svg";
 import { RiMenu4Fill } from "react-icons/ri";
 import { FiShoppingBag, FiSearch } from "react-icons/fi";
-import NavbarDroprown from "./NavbarDroprown";
+
+import DropdownLarge from "./DropdownLarge";
 
 const links = [
-  { name: "Home" },
   {
-    name: "Courses",
-    submenu: true,
+    Head: "Course List Layouts",
     sublinks: [
-      {
-        Head: "Courses",
-        sublinks: [
-          { name: "Menu1", link: "/" },
-          { name: "Menu2", link: "/" },
-          { name: "Menu3", link: "/" },
-          { name: "Menu4", link: "/" },
-          { name: "Menu5", link: "/" },
-        ],
-      },
-      {
-        Head: "Courses2",
-        sublinks: [
-          { name: "Menu1", link: "/" },
-          { name: "Menu2", link: "/" },
-          { name: "Menu3", link: "/" },
-          { name: "Menu4", link: "/" },
-          { name: "Menu5", link: "/" },
-        ],
-      },
+      { name: "Course List v1", link: "/" },
+      { name: "Course List v2", link: "/" },
+      { name: "Course List v3", link: "/" },
+      { name: "Course List v4", link: "/" },
+      { name: "Course List v5", link: "/" },
     ],
   },
-  { name: "Blog" },
-  { name: "Contact" },
+  {
+    Head: "Course Single Layouts",
+    sublinks: [
+      { name: "Course Single v1", link: "/" },
+      { name: "Course Single v2", link: "/" },
+      { name: "Course Single v3", link: "/" },
+      { name: "Course Single v4", link: "/" },
+      { name: "Course Single v5", link: "/" },
+    ],
+  },
+  {
+    Head: "About Courses",
+    sublinks: [
+      { name: "Lesson Page v1", link: "/" },
+      { name: "Lesson Page v2", link: "/" },
+      { name: "Lesson Page v3", link: "/" },
+      { name: "Lesson Page v4", link: "/" },
+      { name: "Lesson Page v5", link: "/" },
+    ],
+  },
+  {
+    Head: "Dashboard Pages",
+    sublinks: [
+      { name: "Dashboard ", link: "/" },
+      { name: "My Courses ", link: "/" },
+      { name: "Bookmarks ", link: "/" },
+      { name: "Add Listing ", link: "/" },
+      { name: "Reviews", link: "/" },
+    ],
+  },
+  {
+    Head: "Popular Courses",
+    sublinks: [
+      { name: "Web Developer ", link: "/" },
+      { name: "Mobile Developer ", link: "/" },
+      { name: "Digital Marketing ", link: "/" },
+      { name: "Development ", link: "/" },
+      { name: "Finance & Accounting", link: "/" },
+    ],
+  },
 ];
 
-const Header: FC = () => {
-  const [open, setOpen] = useState(false);
+const Header: FC = () => (
+  <div className="w-full bg-head text-white sticky top-0 z-[50]">
+    <div className="container py-5 border-b border-b-white/[.15] flex items-center justify-between">
+      <div className="flex items-center gap-7">
+        <Link href="/">
+          <Image src={mainLogo} alt="IntelliSense" />
+        </Link>
+        <button className="text-color-6 flex items-center gap-2 text-md-regular hover:text-color-6/70 duration-300 group">
+          <RiMenu4Fill size={24} />
+          <span>Explore</span>
+          <ul>
+            <DropdownLarge items={links} />
+          </ul>
+        </button>
+      </div>
 
-  return (
-    <>
-      <div className="w-full bg-head text-white sticky top-0 z-[50]">
-        <div className="container py-5 border-b border-b-white/[.15] flex items-center justify-between">
-          <div className="flex items-center gap-7">
-            <Link href="/">
-              <Image src={mainLogo} alt="IntelliSense" />
-            </Link>
-            <button className="text-color-6 flex items-center gap-2 text-md-regular hover:text-color-6/70 duration-300">
-              <RiMenu4Fill size={24} />
-              <span>Explore</span>
-            </button>
-          </div>
-
-          <nav className="">
-            <ul className="flex items-center gap-10 text-md-regular">
-              {links.map((link, index) => (
-                <li
-                  key={`navbar-menu-item-${index}`}
-                  className="hover:text-white/70 duration-300 group"
-                >
-                  {link.name}
-                  {link.submenu && <NavbarDroprown links={link.sublinks} />}
-                </li>
-              ))}
-            </ul>
-          </nav>
-
-          <div className="flex items-center gap-7">
-            <button className="text-xl hover:opacity-70 duration-300">
-              <FiSearch />
-            </button>
-            <button className="text-xl hover:opacity-70 duration-300">
-              <FiShoppingBag />
-            </button>
-
-            <button className="text-white text-md-regular hover:text-white/70 duration-300">
-              Log In
-            </button>
 
             <button className="text-head bg-white rounded-lg px-[34px] py-2 text-md-regular hover:bg-white/70 duration-300">
               Sign Up
