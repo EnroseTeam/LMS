519ef3384d1c885fddd86f334b98e2cc2b26f265
Javkhlant Altankhuyag <enroseking95@gmail.com>
2023-04-22 15:37:51 +0800

    Added rating filter and sorting to courses

--- client/src/components/global/NavbarDroprown.tsx
+++ client/src/components/global/NavbarDroprown.tsx
@@ -15,37 +15,28 @@ interface NavbarDroprownProps {
   links: SubLink[];
 }
 
-const NavbarDroprown: FC<NavbarDroprownProps> = ({ links }) => {
-  return (
-    <>
-      <div>
-        <div className="absolute top-15 hidden group-hover:block hover:block">
-          <div className="py-3">
-            <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45" />
-          </div>
-          <div className="bg-white text-head p-3.5 grid grid-cols-2 gap-10">
-            {links.map((mysublinks, index) => (
-              <div key={`dropdown-${index}`}>
-                <h1 className="hover:text-white/70 duration-300">
-                  {mysublinks.Head}
-                </h1>
-                {mysublinks.sublinks.map((slink, index) => (
-                  <li
-                    className="hover:text-white/70 duration-300"
-                    key={`mysublinks-${index}`}
-                  >
-                    <Link href={slink.link} className="hover:text-color-6">
-                      {slink.name}
-                    </Link>
-                  </li>
-                ))}
-              </div>
+const NavbarDroprown: FC<NavbarDroprownProps> = ({ links }) => (
+  <div>
+    <div className="absolute top-15 hidden group-hover:block hover:block">
+      <div className="py-3">
+        <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45" />
+      </div>
+      <ul className="bg-white text-head p-3.5 grid grid-cols-2 gap-10">
+        {links.map((mysublinks, index) => (
+          <div key={`dropdown-${index}`}>
+            <h1 className="hover:text-white/70 duration-300">{mysublinks.Head}</h1>
+            {mysublinks.sublinks.map((slink, index) => (
+              <li className="hover:text-white/70 duration-300" key={`mysublinks-${index}`}>
+                <Link href={slink.link} className="hover:text-color-6">
+                  {slink.name}
+                </Link>
+              </li>
             ))}
           </div>
-        </div>
-      </div>
-    </>
-  );
-};
+        ))}
+      </ul>
+    </div>
+  </div>
+);
 
 export default NavbarDroprown;
--- client/src/components/global/RadioButtonFilter.tsx
+++ client/src/components/global/RadioButtonFilter.tsx
@@ -1,23 +1,39 @@
-import { FC, useState } from "react";
+import { IRadioButtonFilterItem } from "@/interfaces/components";
+import { useRouter } from "next/router";
+import { FC, useEffect, useState } from "react";
 
 import { BsChevronDown } from "react-icons/bs";
 
-interface Item {
-  content?: JSX.Element | string;
-  count?: number;
-}
-
 interface RadioButtonFilterProps {
-  title: string;
-  items?: Item[];
+  title: {
+    name: string;
+    slug: string;
+  };
+  items?: IRadioButtonFilterItem[];
 }
 
-const RadioButtonFilter: FC<RadioButtonFilterProps> = ({
-  title,
-  items = [],
-}) => {
+const RadioButtonFilter: FC<RadioButtonFilterProps> = ({ title, items = [] }) => {
+  const router = useRouter();
+
   const [show, setShow] = useState(true);
   const [displayItems, setDisplayItems] = useState(items.slice(0, 5));
+
+  const [selected, setSelected] = useState<string>(
+    router.query[title.slug.toLowerCase()] !== undefined
+      ? (router.query[title.slug.toLowerCase()] as string)
+      : ""
+  );
+
+  useEffect(() => {
+    if (selected) {
+      router.push({
+        query: { ...router.query, [title.slug.toLowerCase()]: selected },
+      });
+    } else {
+      delete router.query[title.slug.toLowerCase()];
+      router.push({ query: router.query });
+    }
+  }, [selected]);
 
   return (
     <div>
@@ -25,7 +41,7 @@ const RadioButtonFilter: FC<RadioButtonFilterProps> = ({
         onClick={(): void => setShow(!show)}
         className="flex w-full justify-between items-center text-head font-medium leading-[23px] text-[20px]"
       >
-        {title}
+        {title.name}
         <BsChevronDown
           size={15}
           className={`${show ? "rotate-[-180deg]" : "rotate-0"} duration-300`}
@@ -33,9 +49,7 @@ const RadioButtonFilter: FC<RadioButtonFilterProps> = ({
       </button>
       <div
         className={`${
-          show
-            ? "max-h-[1000px] opacity-100 py-[30px]"
-            : "max-h-0 opacity-0 pointer-events-none"
+          show ? "max-h-[1000px] opacity-100 py-[30px]" : "max-h-0 opacity-0 pointer-events-none"
         } z-10 border-b duration-150`}
       >
         {displayItems.map((item, index) => (
@@ -46,32 +60,48 @@ const RadioButtonFilter: FC<RadioButtonFilterProps> = ({
             <div className="flex items-center gap-[15px]">
               <input
                 type="radio"
+                value={item.slug}
+                checked={item.slug === selected}
+                onChange={(e): void => {
+                  setSelected(e.target.value);
+                }}
                 id={`button-${index}`}
                 className="w-[15px] h-[15px] border-2 border-2-icon text-black"
-                name="default-radio"
+                name={title.slug}
               />
-              <label
-                htmlFor={`button-${index}`}
-                className="select-none w-[20ch]"
-              >
+              <label htmlFor={`button-${index}`} className="select-none w-[20ch]">
                 {item.content}
               </label>
             </div>
             <div className="text-text select-none">({item.count})</div>
           </div>
         ))}
-        {items.length > 5 && (
+        <div
+          className={`flex items-center ${
+            items.length > 5 ? "justify-between" : "justify-end"
+          } w-full mt-[22px]`}
+        >
+          {items.length > 5 && (
+            <button
+              className="text-sm-medium text-color-1 underline"
+              onClick={(): void =>
+                displayItems.length > 5
+                  ? setDisplayItems(items.slice(0, 5))
+                  : setDisplayItems(items)
+              }
+            >
+              {displayItems.length > 5 ? "Show Less" : "Show More"}
+            </button>
+          )}
           <button
-            className="text-sm-medium text-color-1 underline mt-[22px]"
-            onClick={(): void =>
-              displayItems.length > 5
-                ? setDisplayItems(items.slice(0, 5))
-                : setDisplayItems(items)
-            }
+            onClick={(): void => {
+              setSelected("");
+            }}
+            className="text-sm-medium text-color-1 underline"
           >
-            {displayItems.length > 5 ? "Show Less" : "Show More"}
+            Цэвэрлэх
           </button>
-        )}
+        </div>
       </div>
     </div>
   );
