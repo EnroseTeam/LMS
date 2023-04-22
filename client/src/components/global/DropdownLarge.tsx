1bc294ce3770b6d7ea1d4f9dc000913d90ee98c5
Ganbold02 <115781781+Ganbold02@users.noreply.github.com>
2023-04-22 15:53:50 +0800

    DropdownLarge

---
+++ client/src/components/global/DropdownLarge.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import React, { FC } from "react";
+
+interface SubLinks {
+  name: string;
+  link: string;
+}
+
+interface DropDownLargeItem {
+  Head: string;
+  sublinks: SubLinks[];
+}
+
+interface DropdownLargeProps {
+  items: DropDownLargeItem[];
+}
+
+const DropdownLarge: FC<DropdownLargeProps> = ({ items }) => (
+  <>
+    <div>
+      <div className="absolute top-15 hidden group-hover:block hover:block w-[1390px] left-[50%] -translate-x-[50%] rounded-lg">
+        {/* <div className="py-3">
+          <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45" />
+        </div> */}
+        <div className="bg-white/95 text-head p-3.5 grid grid-cols-5 gap-10 mt-10">
+          {items.map((mysublinks, index) => (
+            <div key={`dropdown-${index}`}>
+              <h1 className="hover:text-head duration-300 text-left pb-5 uppercase">
+                {mysublinks.Head}
+              </h1>
+              {mysublinks.sublinks.map((slink, index) => (
+                <li
+                  className="hover:text-head duration-300 text-left pb-3"
+                  key={`mysublinks-${index}`}
+                >
+                  <Link href={slink.link} className="hover:text-head">
+                    {slink.name}
+                  </Link>
+                </li>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  </>
+);
+
+export default DropdownLarge;
--- client/src/components/global/Header.tsx
+++ client/src/components/global/Header.tsx
@@ -1,10 +1,64 @@
-import { FC } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import { FC } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-import mainLogo from '@/assets/logo-main.svg';
-import { RiMenu4Fill } from 'react-icons/ri';
-import { FiShoppingBag, FiSearch } from 'react-icons/fi';
+import mainLogo from "@/assets/logo-main.svg";
+import { RiMenu4Fill } from "react-icons/ri";
+import { FiShoppingBag, FiSearch } from "react-icons/fi";
+import DropdownLarge from "./DropdownLarge";
+
+const links = [
+  {
+    Head: "Course List Layouts",
+    sublinks: [
+      { name: "Course List v1", link: "/" },
+      { name: "Course List v2", link: "/" },
+      { name: "Course List v3", link: "/" },
+      { name: "Course List v4", link: "/" },
+      { name: "Course List v5", link: "/" },
+    ],
+  },
+  {
+    Head: "Course Single Layouts",
+    sublinks: [
+      { name: "Course Single v1", link: "/" },
+      { name: "Course Single v2", link: "/" },
+      { name: "Course Single v3", link: "/" },
+      { name: "Course Single v4", link: "/" },
+      { name: "Course Single v5", link: "/" },
+    ],
+  },
+  {
+    Head: "About Courses",
+    sublinks: [
+      { name: "Lesson Page v1", link: "/" },
+      { name: "Lesson Page v2", link: "/" },
+      { name: "Lesson Page v3", link: "/" },
+      { name: "Lesson Page v4", link: "/" },
+      { name: "Lesson Page v5", link: "/" },
+    ],
+  },
+  {
+    Head: "Dashboard Pages",
+    sublinks: [
+      { name: "Dashboard ", link: "/" },
+      { name: "My Courses ", link: "/" },
+      { name: "Bookmarks ", link: "/" },
+      { name: "Add Listing ", link: "/" },
+      { name: "Reviews", link: "/" },
+    ],
+  },
+  {
+    Head: "Popular Courses",
+    sublinks: [
+      { name: "Web Developer ", link: "/" },
+      { name: "Mobile Developer ", link: "/" },
+      { name: "Digital Marketing ", link: "/" },
+      { name: "Development ", link: "/" },
+      { name: "Finance & Accounting", link: "/" },
+    ],
+  },
+];
 
 const Header: FC = () => (
   <div className="w-full bg-head text-white sticky top-0 z-[50]">
@@ -13,9 +67,12 @@ const Header: FC = () => (
         <Link href="/">
           <Image src={mainLogo} alt="IntelliSense" />
         </Link>
-        <button className="text-color-6 flex items-center gap-2 text-md-regular hover:text-color-6/70 duration-300">
+        <button className="text-color-6 flex items-center gap-2 text-md-regular hover:text-color-6/70 duration-300 group">
           <RiMenu4Fill size={24} />
           <span>Explore</span>
+          <ul>
+            <DropdownLarge items={links} />
+          </ul>
         </button>
       </div>
 
