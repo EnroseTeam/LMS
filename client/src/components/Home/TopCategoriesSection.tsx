57a0352bd6b6b401e5acdbe484a49af50845f812
Javkhlant Altankhuyag <enroseking95@gmail.com>
2023-04-23 16:54:45 +0800

    Made topcategories responsive

--- client/src/components/Home/TopCategoriesSection.tsx
+++ client/src/components/Home/TopCategoriesSection.tsx
@@ -1,14 +1,14 @@
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-import { FC } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper';
+import { FC } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
 
-import CategoryCard from './subComponents/CategoryCard';
-import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
-import { ICourseCategory } from '@/interfaces/courses';
+import CategoryCard from "./subComponents/CategoryCard";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import { ICourseCategory } from "@/interfaces/courses";
 
 interface TopCategoriesProps {
   categories: ICourseCategory[];
@@ -17,19 +17,27 @@ interface TopCategoriesProps {
 const TopCategoriesSection: FC<TopCategoriesProps> = ({ categories }) => (
   <div className="container mb-[120px]">
     <div className="mb-[51px] text-center">
-      <h1 className="text-head text-3xl-bold mb-[9px]">Top Categories</h1>
-      <p className="text-text text-md-regular">Lorem ipsum dolor sit amet, consectetur.</p>
+      <h1 className="text-head text-3xl-bold mb-[9px]">Сургалтын ангилалууд</h1>
+      <p className="text-text text-md-regular">
+        Хүссэн сургалтаа ангилалаар нь шүүгээд олох боломжтой.
+      </p>
     </div>
 
     <Swiper
       grabCursor={true}
-      spaceBetween={30}
-      slidesPerView={6}
-      navigation={{
-        nextEl: '.slider-style-2-next',
-        prevEl: '.slider-style-2-prev',
+      spaceBetween={20}
+      slidesPerView={2}
+      breakpoints={{
+        1024: {
+          slidesPerView: 6,
+          spaceBetween: 30,
+        },
       }}
-      pagination={{ clickable: true, el: '.slider-style-2-pagination' }}
+      navigation={{
+        nextEl: ".slider-style-2-next",
+        prevEl: ".slider-style-2-prev",
+      }}
+      pagination={{ clickable: true, el: ".slider-style-2-pagination" }}
       modules={[Navigation, Pagination]}
     >
       {categories.map((category) => (
@@ -38,7 +46,7 @@ const TopCategoriesSection: FC<TopCategoriesProps> = ({ categories }) => (
         </SwiperSlide>
       ))}
 
-      <div className="flex items-center justify-center gap-5 mt-[60px]">
+      <div className="flex items-center justify-center gap-5 mt-[30px] lg:mt-[60px]">
         <button className="slider-style-2-prev">
           <BsArrowLeft />
         </button>
