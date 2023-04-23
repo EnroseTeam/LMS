7a49ea3ba9cf25a8d5f3861bd269f8b99e9265b2
binderiyats <binderiyatsogthers@gmail.com>
2023-04-23 14:54:28 +0800

    Instructors page was translated to the mongolian, and made it dynamical

--- client/src/pages/instructors/[id].tsx
+++ client/src/pages/instructors/[id].tsx
@@ -9,37 +9,44 @@ import Breadcrumbs from "@/components/global/Breadcrumbs";
 import BgShape from "../../assets/hero-shape.svg";
 import Image from "next/image";
 import SinglePageContent from "@/components/Instructors/SinglePageContent";
+import { IUser } from "@/interfaces/user";
 
 interface SingleInstructorPageProps {
   categories: ICourseCategory[];
   courses: ICourse[];
+  instructor: IUser;
 }
 
 export const getServerSideProps: GetServerSideProps<
   SingleInstructorPageProps
-> = async () => {
-  const [categoryRes, courseRes] = await axios.all([
+> = async ({ params }) => {
+  const [categoryRes, courseRes, instructorRes] = await axios.all([
     axios.get("http://localhost:5000/api/courses/categories"),
     axios.get("http://localhost:5000/api/courses"),
+    axios.get(`http://localhost:5000/api/users/${params?.id}`),
   ]);
   return {
     props: {
       categories: categoryRes.data.body,
       courses: courseRes.data.body,
+      instructor: instructorRes.data.body,
     },
   };
 };
 
-const SingleInstructorPage: FC<SingleInstructorPageProps> = ({ courses }) => (
+const SingleInstructorPage: FC<SingleInstructorPageProps> = ({
+  courses,
+  instructor,
+}) => (
   <div>
     <Breadcrumbs
       breadcrumbItems={[
         { title: "Багш, сургагч нар", link: "/instructors" },
-        { title: "Ali Tufan", link: "/instructors/instructor" },
+        { title: instructor.fullName, link: `/instructors/${instructor._id}` },
       ]}
     />
 
-    <SinglePageHeader />
+    <SinglePageHeader instructor={instructor} />
 
     <div className="absolute container top-8 right-0 left-0 pointer-events-none ">
       <Image src={BgShape} alt="" className="w-full" />
