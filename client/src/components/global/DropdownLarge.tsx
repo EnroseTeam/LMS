86c9ed8a040d839eebcf1814781c00fe7f5afb57
Javkhlant Altankhuyag <enroseking95@gmail.com>
2023-04-23 13:52:03 +0800

    Fixed the navbar dropdown

--- client/src/components/global/DropdownLarge.tsx
+++ client/src/components/global/DropdownLarge.tsx
@@ -22,7 +22,7 @@ const DropdownLarge: FC<DropdownLargeProps> = ({ items }) => (
         {/* <div className="py-3">
           <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45" />
         </div> */}
-        <div className="bg-white/95 text-head p-3.5 grid grid-cols-5 gap-10 mt-10">
+        <ul className="bg-white/95 text-head p-3.5 grid grid-cols-5 gap-10 mt-10">
           {items.map((mysublinks, index) => (
             <div key={`dropdown-${index}`}>
               <h1 className="hover:text-head duration-300 text-left pb-5 uppercase">
@@ -40,7 +40,7 @@ const DropdownLarge: FC<DropdownLargeProps> = ({ items }) => (
               ))}
             </div>
           ))}
-        </div>
+        </ul>
       </div>
     </div>
   </>
