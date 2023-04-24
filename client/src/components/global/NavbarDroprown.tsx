f53f44b5e4320ce1510303e5b0537d4c816d0510
Javkhlant Altankhuyag <enroseking95@gmail.com>
2023-04-24 12:22:18 +0800

    Improved navbardropdown

--- client/src/components/global/NavbarDroprown.tsx
+++ client/src/components/global/NavbarDroprown.tsx
@@ -1,39 +1,58 @@
 import Link from "next/link";
 import React, { FC } from "react";
 
-interface SubLinkChild {
-  name: string;
-  link: string;
-}
-
-interface SubLink {
-  Head: string;
-  sublinks: SubLinkChild[];
-}
-
-interface NavbarDroprownProps {
-  links: SubLink[];
-}
-
-const NavbarDroprown: FC<NavbarDroprownProps> = ({ links }) => (
-  <div>
-    <div className="absolute top-15 hidden group-hover:block hover:block">
-      <div className="py-3">
-        <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45" />
-      </div>
-      <ul className="bg-white text-head p-3.5 grid grid-cols-2 gap-10">
-        {links.map((mysublinks, index) => (
-          <div key={`dropdown-${index}`}>
-            <h1 className="hover:text-white/70 duration-300">{mysublinks.Head}</h1>
-            {mysublinks.sublinks.map((slink, index) => (
-              <li className="hover:text-white/70 duration-300" key={`mysublinks-${index}`}>
-                <Link href={slink.link} className="hover:text-color-6">
-                  {slink.name}
-                </Link>
-              </li>
-            ))}
-          </div>
-        ))}
+const NavbarDroprown: FC = () => (
+  <div className="absolute top-10 left-0 opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto duration-300">
+    <div className="px-7">
+      <div className="w-[10px] h-[10px] bg-white rotate-45 " />
+    </div>
+    <div className="bg-white max-content pl-[30px] pr-[76px] py-6 rounded-lg shadow-shadow-4 -mt-[5px] ">
+      <ul className="whitespace-nowrap text-head text-md-regular leading-[35px]">
+        <li>
+          <Link className="hover:text-color-1 hover:underline duration-300" href="/">
+            About us
+          </Link>
+        </li>
+        <li>
+          <Link className="hover:text-color-1 hover:underline duration-300" href="/">
+            About us
+          </Link>
+        </li>
+        <li>
+          <Link className="hover:text-color-1 hover:underline duration-300" href="/">
+            About us
+          </Link>
+        </li>
+        <li>
+          <Link className="hover:text-color-1 hover:underline duration-300" href="/">
+            About us
+          </Link>
+        </li>
+        <li>
+          <Link className="hover:text-color-1 hover:underline duration-300" href="/">
+            About us
+          </Link>
+        </li>
+        <li>
+          <Link className="hover:text-color-1 hover:underline duration-300" href="/">
+            About us
+          </Link>
+        </li>
+        <li>
+          <Link className="hover:text-color-1 hover:underline duration-300" href="/">
+            About us
+          </Link>
+        </li>
+        <li>
+          <Link className="hover:text-color-1 hover:underline duration-300" href="/">
+            About us
+          </Link>
+        </li>
+        <li>
+          <Link className="hover:text-color-1 hover:underline duration-300" href="/">
+            About us
+          </Link>
+        </li>
       </ul>
     </div>
   </div>
