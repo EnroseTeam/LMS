281c7448a760ef1e74f72c311eff61383b7ebe9a
binderiya <89821684+binderiyats@users.noreply.github.com>
2023-04-25 12:19:59 +0800

    Added searchbar and made it working (#168)
    Co-authored-by: Javkhlant <110245204+EnRoseK@users.noreply.github.com>

--- client/src/components/global/Header.tsx
+++ client/src/components/global/Header.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -9,21 +9,29 @@ import { FiShoppingBag, FiSearch } from "react-icons/fi";
 
 import NavbarDropdownLarge from "./NavbarDropdownLarge";
 import NavbarDroprown from "./NavbarDroprown";
+import SearchBar from "../Search/SearchBar";
 
-const Header: FC = () => (
-  <div className="w-full bg-head text-white sticky top-0 z-[50]">
-    <div className="container py-5 border-b border-b-white/[.15] flex items-center justify-between">
-      <div className="flex items-center gap-7">
-        <Link href="/">
-          <Image src={mainLogo} alt="IntelliSense" />
-        </Link>
-        <div className="py-2 px-2 hover:bg-white/[.15] rounded-lg text-color-6 flex items-center gap-2 text-md-regular hover:text-color-6/70 duration-300 group relative cursor-pointer">
+const Header: FC = () => {
+  const [searchBarShow, setSearchBarShow] = useState<boolean>(false);
+
+  const showSearchBar = (): void => {
+    setSearchBarShow(true);
+  };
+
+  return (
+    <div className="w-full bg-head text-white sticky top-0 z-[50]">
+      <div className="container py-5 border-b border-b-white/[.15] flex items-center justify-between">
+        <div className="flex items-center gap-7">
+          <Link href="/">
+            <Image src={mainLogo} alt="IntelliSense" />
+          </Link>
+          <div className="py-2 px-2 hover:bg-white/[.15] rounded-lg text-color-6 flex items-center gap-2 text-md-regular hover:text-color-6/70 duration-300 group relative cursor-pointer">
           <RiMenu4Fill size={24} />
           <span>Explore</span>
           <NavbarDropdownLarge />
         </div>
-      </div>
-      <nav className="hidden lg:block">
+        </div>
+         <nav className="hidden lg:block">
         <ul className="flex items-center gap-1 text-md-regular">
           <li className="py-2 px-4 hover:text-color-6 hover:bg-white/[.15] rounded-lg duration-300 group relative">
             <Link href="/">Нүүр хуудас</Link>
@@ -47,14 +55,17 @@ const Header: FC = () => (
           </li>
         </ul>
       </nav>
-      <div className="hidden items-center gap-6 lg:flex">
-        <button className="text-xl hover:opacity-70 duration-300">
-          <FiSearch />
-        </button>
-        <button className="text-xl hover:opacity-70 duration-300">
-          <FiShoppingBag />
-        </button>
-        <Link
+        <div className="flex items-center gap-7">
+          <button
+            className="text-xl hover:opacity-70 duration-300"
+            onClick={showSearchBar}
+          >
+            <FiSearch />
+          </button>
+          <button className="text-xl hover:opacity-70 duration-300">
+            <FiShoppingBag />
+          </button>
+            <Link
           href="/auth/login"
           className="text-white text-md-regular hover:text-white/70 duration-300"
         >
@@ -66,9 +77,15 @@ const Header: FC = () => (
         >
           Бүртгүүлэх
         </Link>
+        </div>
+
       </div>
+      <SearchBar
+        searchBarShow={searchBarShow}
+        setSearchBarShow={setSearchBarShow}
+      />
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
