bf01b1eccb0bd1b5e811e53e446f4ad87dd35ec6
binderiyats <binderiyatsogthers@gmail.com>
2023-04-25 14:20:02 +0800

    Added backdrop and made it pretty

--- client/src/components/Search/SearchBar.tsx
+++ client/src/components/Search/SearchBar.tsx
@@ -10,65 +10,77 @@ interface SearchBarProps {
 }
 
 const SearchBar: FC<SearchBarProps> = ({ searchBarShow, setSearchBarShow }) => (
-  <div
-    className={`${
-      searchBarShow ? "opacity-100" : "opacity-0 pointer-events-none"
-    } fixed w-screen bg-white top-0 text-head duration-150`}
-  >
-    <div className="max-w-[1390px] mx-auto mt-[100px] mb-[97px]">
-      <div className="relative flex gap-4 justify-between items-center border-b mb-[51px]">
-        <FiSearch className="text-icon" size={25} />
-        <input
-          type="text"
-          placeholder="Та юу сурахыг хүсэж байна вэ?"
-          className="w-full h-full py-[19px] focus:outline-none text-xl-medium placeholder:text-head"
-        />
-        <button
-          className="absolute right-0 text-icon"
-          onClick={(): void => {
-            setSearchBarShow(false);
-          }}
-        >
-          <RxCross2 size={25} />
-        </button>
-      </div>
-      <h1 className="text-lg-medium mb-5">Яг одоо эрэлттэй</h1>
-      <ul className="mb-[30px]">
-        <li className="text-[15px] font-normal leading-[35px] hover:font-medium hover:text-color-1 hover:underline">
-          <Link href="#">
-            The Ultimate Drawing Course - Beginner to Advanced
-          </Link>
-        </li>
-        <li className="text-[15px] font-normal leading-[35px] hover:font-medium hover:text-color-1 hover:underline">
-          <Link href="#">
-            The Ultimate Drawing Course - Beginner to Advanced
-          </Link>
-        </li>
-        <li className="text-[15px] font-normal leading-[35px] hover:font-medium hover:text-color-1 hover:underline">
-          <Link href="#">
-            The Ultimate Drawing Course - Beginner to Advanced
-          </Link>
-        </li>
-        <li className="text-[15px] font-normal leading-[35px] hover:font-medium hover:text-color-1 hover:underline">
-          <Link href="#">
-            The Ultimate Drawing Course - Beginner to Advanced
-          </Link>
-        </li>
-        <li className="text-[15px] font-normal leading-[35px] hover:font-medium hover:text-color-1 hover:underline">
-          <Link href="#">
-            The Ultimate Drawing Course - Beginner to Advanced
-          </Link>
-        </li>
-      </ul>
+  <>
+    <div
+      className={`${
+        searchBarShow
+          ? "opacity-100 top-0"
+          : "opacity-0 pointer-events-none top-[-100%]"
+      } fixed w-screen bg-white text-head duration-150 z-[9999]`}
+    >
+      <div className="max-w-[1390px] mx-auto mt-[100px] mb-[97px]">
+        <div className="relative flex gap-4 justify-between items-center border-b mb-[51px]">
+          <FiSearch className="text-icon" size={25} />
+          <input
+            type="text"
+            placeholder="Та юу сурахыг хүсэж байна вэ?"
+            className="w-full h-full py-[19px] focus:outline-none text-xl-medium placeholder:text-head"
+          />
+          <button
+            className="absolute right-0 text-icon"
+            onClick={(): void => {
+              setSearchBarShow(false);
+            }}
+          >
+            <RxCross2 size={25} />
+          </button>
+        </div>
+        <h1 className="text-lg-medium mb-5">Яг одоо эрэлттэй</h1>
+        <ul className="mb-[30px] max-content">
+          <li className="text-[15px] font-normal leading-[35px] hover:font-medium hover:text-color-1 hover:underline">
+            <Link href="#">
+              The Ultimate Drawing Course - Beginner to Advanced
+            </Link>
+          </li>
+          <li className="text-[15px] font-normal leading-[35px] hover:font-medium hover:text-color-1 hover:underline">
+            <Link href="#">
+              The Ultimate Drawing Course - Beginner to Advanced
+            </Link>
+          </li>
+          <li className="text-[15px] font-normal leading-[35px] hover:font-medium hover:text-color-1 hover:underline">
+            <Link href="#">
+              The Ultimate Drawing Course - Beginner to Advanced
+            </Link>
+          </li>
+          <li className="text-[15px] font-normal leading-[35px] hover:font-medium hover:text-color-1 hover:underline">
+            <Link href="#">
+              The Ultimate Drawing Course - Beginner to Advanced
+            </Link>
+          </li>
+          <li className="text-[15px] font-normal leading-[35px] hover:font-medium hover:text-color-1 hover:underline">
+            <Link href="#">
+              The Ultimate Drawing Course - Beginner to Advanced
+            </Link>
+          </li>
+        </ul>
 
-      <Link
-        href="#"
-        className="text-text font-medium [text-15px] leading-[35px] underline"
-      >
-        PRESS ENTER TO SEE ALL SEARCH RESULTS
-      </Link>
+        <Link
+          href="#"
+          className="text-text font-medium [text-15px] leading-[35px] hover:text-color-1 hover:underline"
+        >
+          PRESS ENTER TO SEE ALL SEARCH RESULTS
+        </Link>
+      </div>
     </div>
-  </div>
+    <div
+      className={`${
+        searchBarShow ? "opacity-50" : "opacity-0 pointer-events-none"
+      } fixed w-screen h-screen bg-black top-0 left-0 right-0 bottom-0 duration-150`}
+      onClick={(): void => {
+        setSearchBarShow(false);
+      }}
+    />
+  </>
 );
 
 export default SearchBar;
