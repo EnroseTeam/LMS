1ccee83eb743e6ab6d33f64b50d16387b53c4a65
Javkhlant Altankhuyag <enroseking95@gmail.com>
2023-04-26 08:33:30 +0800

    Added password, email and phone validator on signup controller

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -7,20 +7,10 @@ import UserRoleModel from "../models/userRole";
 interface SignUpBody {
   firstName?: string;
   lastName?: string;
-  birthDate?: string;
   email?: string;
   phone?: string;
-  address?: string;
-  avatar?: string;
   password?: string;
-  role?: string;
-  socialAccounts?: {
-    facebook?: string;
-    linkedin?: string;
-    twitter?: string;
-    instagram?: string;
-  };
-  bio?: string;
+  rePassword?: string;
 }
 
 interface LogInBody {
@@ -35,7 +25,14 @@ export const signUp: RequestHandler<
   SignUpBody,
   unknown
 > = async (req, res, next) => {
-  const { firstName, lastName, email, phone, password: rawPassword } = req.body;
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    password: rawPassword,
+    rePassword,
+  } = req.body;
   try {
     if (!firstName)
       throw createHttpError(400, "Хэрэглэгчийн нэр заавал шаардлагатай.");
@@ -47,6 +44,22 @@ export const signUp: RequestHandler<
       throw createHttpError(400, "Хэрэглэгчийн утас заавал шаардлагатай.");
     if (!rawPassword)
       throw createHttpError(400, "Хэрэглэгчийн нууц үг заавал шаардлагатай.");
+    if (rawPassword !== rePassword)
+      throw createHttpError(400, "Нууц үг таарахгүй байна.");
+
+    const passwordChecker = new RegExp(
+      "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})"
+    );
+    if (!passwordChecker.test(rawPassword))
+      throw createHttpError(400, "Нууц үг шаардлага хангахгүй байна.");
+
+    const emailChecker = new RegExp(/[^\s@]+@[^\s@]+\.[^\s@]+/gi, "gm");
+    if (!emailChecker.test(email))
+      throw createHttpError(400, "И-мэйл хаяг буруу байна.");
+
+    const phoneChecker = new RegExp("^[89]\\d{7}$");
+    if (!phoneChecker.test(phone))
+      throw createHttpError(400, "Утасны дугаар буруу байна.");
 
     const isEmailExist = await UserModel.findOne({ email });
     if (isEmailExist)
@@ -77,9 +90,8 @@ export const signUp: RequestHandler<
 
     req.session.userId = newUser._id;
 
-    res.status(201).json({ message: "Амжилттай бүртгэгдлээ", body: newUser });
+    res.status(201).json({ message: "Амжилттай бүртгэгдлээ" });
   } catch (error) {
-    console.log(error);
     next(error);
   }
 };
@@ -111,7 +123,7 @@ export const logIn: RequestHandler<
 
     req.session.userId = user._id;
 
-    res.status(200).json({ message: "Амжилттай нэвтэрлээ", body: user });
+    res.status(200).json({ message: "Амжилттай нэвтэрлээ" });
   } catch (error) {
     next(error);
   }
