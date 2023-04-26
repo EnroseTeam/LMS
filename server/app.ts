a89e0f9a1d977395ab910f80b375a70f105c7607
Javkhlant Altankhuyag <enroseking95@gmail.com>
2023-04-26 09:18:12 +0800

    Added login and register funciton to login and register pages.

--- server/app.ts
+++ server/app.ts
@@ -20,7 +20,12 @@ import MongoStore from "connect-mongo";
 
 const app: Express = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(
   session({
