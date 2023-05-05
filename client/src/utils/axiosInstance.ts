2c44d1d2644e074752c71d1758d5b20373d2e14e
Javkhlant <enrosek95@gmail.com>
2023-05-05 15:35:54 +0800

    Done.

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_ENDPOINT_URL,
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://api-intellisense.onrender.com/"
+      : "http://localhost:5000",
   withCredentials: true,
 });
