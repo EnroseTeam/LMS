c2d7e4034096660f589253564e7462b6e9ad48da
Javkhlant Altankhuyag <enroseking95@gmail.com>
2023-05-07 20:57:35 +0800

    Added orders page

--- client/src/components/global/TabAlternative.tsx
+++ client/src/components/global/TabAlternative.tsx
@@ -1,39 +1,28 @@
-import { useRouter } from "next/router";
 import { FC, useState } from "react";
 import { TabHeaderItem } from "./Tab";
+import classNames from "classnames";
 
 interface TabAlternativeProps {
   tabHeaders: TabHeaderItem[];
   tabContents: JSX.Element[];
 }
 
-const TabAlternative: FC<TabAlternativeProps> = ({
-  tabHeaders,
-  tabContents,
-}) => {
-  const router = useRouter();
-  const [activeTab, setActiveTab] = useState<string>(
-    (router.query.activeTab as string) || tabHeaders[0].slug
-  );
+const TabAlternative: FC<TabAlternativeProps> = ({ tabHeaders, tabContents }) => {
+  const [activeTab, setActiveTab] = useState<string>(tabHeaders[0].slug);
 
   return (
-    <div id="tab-alternative" className="w-full grid grid-cols-3 scroll-mt-32">
+    <div className="w-full grid grid-cols-3">
       <div className="flex flex-col items-start text-head text-xl-medium">
         {tabHeaders.map((tab, index) => (
           <button
             key={`single-course-tab-${index}`}
             onClick={(): void => {
-              router.push({
-                query: { ...router.query, activeTab: tab.slug },
-                hash: "tab-alternative",
-              });
               setActiveTab(tab.slug);
             }}
-            className={`py-4 border-l border-l-border-1 pl-4 hover:text-color-1 duration-300 ${
-              activeTab === tab.slug
-                ? "border-l-2 border-l-color-1 text-color-1"
-                : ""
-            }`}
+            className={classNames(
+              "py-4 border-l-2 border-l-border-1 pl-4 hover:text-color-1 duration-300",
+              { "border-l-2 border-l-color-1 text-color-1": activeTab === tab.slug }
+            )}
           >
             {tab.name}
           </button>
@@ -41,10 +30,17 @@ const TabAlternative: FC<TabAlternativeProps> = ({
       </div>
 
       <div className="col-span-2">
-        {tabContents.map(
-          (tabContent, index) =>
-            activeTab === tabHeaders[index].slug && tabContent
-        )}
+        {tabContents.map((tabContent, index) => (
+          <div
+            key={`tab-alternative-content-${index}`}
+            className={classNames(
+              { block: activeTab === tabHeaders[index].slug },
+              { hidden: activeTab !== tabHeaders[index].slug }
+            )}
+          >
+            {tabContent}
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ client/src/pages/user/orders/index.tsx
@@ -0,0 +1,96 @@
+import OrderItem from "@/components/Orders/OrderItem";
+import Breadcrumbs from "@/components/global/Breadcrumbs";
+import Tab, { TabHeaderItem } from "@/components/global/Tab";
+import { useAuthenticate } from "@/hooks/useAuthenticate";
+import LoadingScreen from "@/utils/LoadingScreen";
+import { useRouter } from "next/router";
+import { FC, useEffect, useState } from "react";
+
+const UserOrdersPage: FC = () => {
+  const router = useRouter();
+  const { user, isLoading } = useAuthenticate();
+  const [isReady, setIsReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.push("/auth/login");
+    }
+    if (!isLoading && user) {
+      setIsReady(true);
+    }
+  }, [user, isLoading]);
+
+  const HeaderContent = (
+    <div className="grid grid-cols-7 gap-5 pb-5 border-b border-b-border-1 -mt-[20px] text-head text-base-medium">
+      <span>Захиалгын дугаар</span>
+      <span className="col-span-2">Сургалтууд</span>
+      <span>Огноо</span>
+      <span>Нийт үнэ</span>
+      <span>Төлбөрийн төрөл</span>
+      <span>Захиалгын төлөв</span>
+    </div>
+  );
+
+  const AllOrdersContent = (
+    <>
+      {HeaderContent}
+      <div className="flex flex-col gap-5 ">
+        <OrderItem />
+      </div>
+    </>
+  );
+
+  const AcceptedOrdersContent = (
+    <>
+      {HeaderContent}
+      <div className="flex flex-col gap-5 ">
+        <OrderItem />
+      </div>
+    </>
+  );
+
+  const PendingOrdersContent = (
+    <>
+      {HeaderContent}
+      <div className="flex flex-col gap-5 ">
+        <OrderItem />
+      </div>
+    </>
+  );
+
+  const tabHeaders: TabHeaderItem[] = [
+    { name: "Бүгд", slug: "all" },
+    { name: "Баталгаажсан", slug: "accepted" },
+    { name: "Хүлээгдэж буй", slug: "pending" },
+  ];
+
+  const tabContents: JSX.Element[] = [
+    AllOrdersContent,
+    AcceptedOrdersContent,
+    PendingOrdersContent,
+  ];
+
+  if (!isReady) return <LoadingScreen />;
+
+  return (
+    <>
+      <Breadcrumbs
+        breadcrumbItems={[
+          { title: "Хэрэглэгч", link: "/user/settings" },
+          { title: "Захиалгууд", link: "/user/orders" },
+        ]}
+      />
+      <div className="container mb-[120px]">
+        <h1 className="text-head text-3xl-bold mb-[9px]">Миний захиалгууд</h1>
+        <p className="text-text text-md-regular mb-[60px]">
+          Миний захиалсан сургалтуудын мэдээлэл.
+        </p>
+        <div className="w-full rounded-2xl shadow-shadow-dashboard p-[30px]">
+          <Tab tabHeaders={tabHeaders} tabContents={tabContents} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserOrdersPage;
