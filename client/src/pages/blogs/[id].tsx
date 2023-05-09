db040641eaa10fae34ae41bc2c37408df2cfd676
Ganbold <u.boldoo@gmail.com>
2023-05-09 16:18:44 +0800

    single page

--- client/src/pages/blogs/[id].tsx
+++ client/src/pages/blogs/[id].tsx
@@ -7,22 +7,43 @@ import Link from "next/link";
 import { ImFacebook, ImLinkedin2, ImTwitter } from "react-icons/im";
 import { BsInstagram } from "react-icons/bs";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import { IBlog } from "@/interfaces/blogs";
+import { GetServerSideProps } from "next/types";
+import { axiosInstance } from "@/utils/axiosInstance";
 
-const SinglePostPage: FC = () => (
+interface SingleBlogPageProps {
+  blogs: IBlog;
+}
+export const getServerSideProps: GetServerSideProps<
+  SingleBlogPageProps
+> = async ({ params }) => {
+  const blogRes = await axiosInstance.get(`/api/blogs/${params?.id}`);
+
+  return {
+    props: {
+      blogs: blogRes.data.body,
+    },
+  };
+};
+
+const SinglePostPage: FC<SingleBlogPageProps> = ({ blogs }) => (
   <>
-    <Breadcrumbs breadcrumbItems={[{ title: "Мэдээ", link: "/blogs" }]} />
+    <Breadcrumbs
+      breadcrumbItems={[
+        { title: "Мэдээ", link: "/blogs" },
+        { title: blogs.name, link: `/blogs/${blogs._id}` },
+      ]}
+    />
     <div className="">
       <div className="max-w-[1100px] mx-auto px-[120px] text-center bg-white">
-        <h1 className="text-head text-4x-bold ">
-          Getting Started The Web Development JavaScript in 2022
-        </h1>
-        <p className="text-lg-regular text-text pb-[90px]">August 10, 2022</p>
+        <h1 className="text-head text-4x-bold ">{blogs.name}</h1>
+        <p className="text-lg-regular text-text pb-[90px]">
+          {new Date(blogs.updatedAt).toLocaleDateString()}
+        </p>
       </div>
       <div className="rounded-lg overflow-hidden group relative px-[315px] ">
         <Image
-          src={
-            "https://team-enrose-s3-bucket.s3.ap-northeast-1.amazonaws.com/images/wdxL7_LiT67eRzHPncQPh-html5-css3.jpg"
-          }
+          src={blogs.picture}
           alt="postSingle"
           width={1290}
           height={600}
@@ -35,18 +56,9 @@ const SinglePostPage: FC = () => (
         <h3 className="text-head text-xl-medium pt-16">
           What makes a good brand book?
         </h3>
-        <p className="py-[30px] text-text text-md-regular">
-          Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Diam
-          phasellus vestibulum lorem sed risus ultricies. Magna sit amet purus
-          gravida quis blandit. Arcu cursus vitae congue mauris. Nunc mattis
-          enim ut tellus elementum sagittis vitae et leo. Semper risus in
-          hendrerit gravida rutrum quisque non. At urna condimentum mattis
-          pellentesque id nibh tortor. A erat nam at lectus urna duis convallis
-          convallis tellus. Sit amet mauris commodo quis imperdiet massa. Vitae
-          congue eu consequat ac felis.
-        </p>
+        <p className="py-[30px] text-text text-md-regular">{blogs.text}</p>
         <div className="py-[30px] text-text text-md-regular">
-          <ul className="flex flex-col gap-[30px]">
+          <ul className="flex flex-col gap-[30px] list-disc">
             <li className="flex items-center gap-[10px]">
               Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida.
             </li>
@@ -58,7 +70,7 @@ const SinglePostPage: FC = () => (
               elementum.{" "}
             </li>
             <li className="flex items-center gap-[10px]">
-              Bibendum est ultricies integer quis. Semper eget duis at tellus.{" "}
+              Bibendum est ultricies integer quis. Semper eget duis at tellus.
             </li>
           </ul>
         </div>
@@ -97,7 +109,7 @@ const SinglePostPage: FC = () => (
             fames.
           </p>
         </div>
-        <div className="flex gap-5">
+        <div className="flex justify-between gap-5">
           <div>
             <Image
               src={
@@ -106,7 +118,7 @@ const SinglePostPage: FC = () => (
               alt="postSingle"
               width={410}
               height={350}
-              className="w-full object-cover aspect-[1.3/1] rounded-2xl"
+              className="w-full object-cover aspect-[1.3] rounded-2xl"
             />
             <p className="text-head text-md-medium pt-[13px]">
               Donec purus posuere nullam lacus aliquam
@@ -267,7 +279,6 @@ const SinglePostPage: FC = () => (
       <p className="text-center text-text text-md-regular pb-[60px]">
         10,000+ unique online course list designs
       </p>
-
       <div className="grid grid-cols-4 gap-6">
         <div>
           <Image
@@ -279,11 +290,11 @@ const SinglePostPage: FC = () => (
             height={350}
             className="w-full object-cover aspect-[1.3/1] rounded-md"
           />
-          <h1>Related Posts</h1>
-          <p>10,000+ unique online course list designs</p>
           <div>
-            <p>EDUCATION</p>
-            <h1>Eco-Education in Our Lives: We Can Change the Future </h1>
+            <p className="text-color-1 text-xs-medium pt-5">EDUCATION</p>
+            <h1 className="text-lg-medium">
+              Eco-Education in Our Lives: We Can Change the Future{" "}
+            </h1>
             <p>December 16, 2022</p>
           </div>
         </div>
@@ -297,11 +308,11 @@ const SinglePostPage: FC = () => (
             height={350}
             className="w-full object-cover aspect-[1.3/1] rounded-md"
           />
-          <h1>Related Posts</h1>
-          <p>10,000+ unique online course list designs</p>
           <div>
-            <p>EDUCATION</p>
-            <h1>Eco-Education in Our Lives: We Can Change the Future </h1>
+            <p className="text-color-1 text-xs-medium pt-5">EDUCATION</p>
+            <h1 className="text-lg-medium">
+              Eco-Education in Our Lives: We Can Change the Future{" "}
+            </h1>
             <p>December 16, 2022</p>
           </div>
         </div>
@@ -315,11 +326,11 @@ const SinglePostPage: FC = () => (
             height={350}
             className="w-full object-cover aspect-[1.3/1] rounded-md"
           />
-          <h1>Related Posts</h1>
-          <p>10,000+ unique online course list designs</p>
           <div>
-            <p>EDUCATION</p>
-            <h1>Eco-Education in Our Lives: We Can Change the Future </h1>
+            <p className="text-color-1 text-xs-medium pt-5">EDUCATION</p>
+            <h1 className="text-lg-medium">
+              Eco-Education in Our Lives: We Can Change the Future{" "}
+            </h1>
             <p>December 16, 2022</p>
           </div>
         </div>
@@ -333,12 +344,12 @@ const SinglePostPage: FC = () => (
             height={350}
             className="w-full object-cover aspect-[1.3/1] rounded-md"
           />
-          <h1>Related Posts</h1>
-          <p>10,000+ unique online course list designs</p>
           <div>
-            <p>EDUCATION</p>
-            <h1>Eco-Education in Our Lives: We Can Change the Future </h1>
-            <p>December 16, 2022</p>
+            <p className=" text-color-1 text-xs-medium pt-5">EDUCATION</p>
+            <h1 className="text-lg-medium">
+              Eco-Education in Our Lives: We Can Change the Future{" "}
+            </h1>
+            <p className="text-sm-regular text-text">December 16, 2022</p>
           </div>
         </div>
       </div>
