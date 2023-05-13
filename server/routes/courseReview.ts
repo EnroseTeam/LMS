45937910eaf5418511d54d95205077454d40c6c0
binderiyats <binderiyatsogthers@gmail.com>
2023-05-13 13:15:32 +0800

    done

--- server/routes/courseReview.ts
+++ server/routes/courseReview.ts
@@ -3,12 +3,20 @@ import {
   createCourseReview,
   deleteCourseReview,
   getCourseReviews,
+  getReviewByInstructorId,
   getSingleCourseReview,
   updateCourseReview,
 } from "../controllers/courseReview";
-import { authenticateUser } from "../middlewares/auth";
+import { authenticateUser, authorizeInstructor } from "../middlewares/auth";
 
 const courseReviewRouter = express.Router();
+
+courseReviewRouter.get(
+  "/instructor",
+  authenticateUser,
+  authorizeInstructor,
+  getReviewByInstructorId
+);
 
 courseReviewRouter.get("/", getCourseReviews);
 
