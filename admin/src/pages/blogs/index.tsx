39c9254e6ce9ed7f0b2dd04d4fced82c03f1f3e2
Binderiya <binderiyatsogthers@gmail.com>
2023-05-21 17:39:42 +0800

    Done

--- admin/src/pages/blogs/index.tsx
+++ admin/src/pages/blogs/index.tsx
@@ -1,162 +1,52 @@
 import { FC } from "react";
-import Image from "next/image";
-import placeholder from "../../assets/placeholder.png";
+import { axiosInstance } from "@/utils/axiosInstance";
 
-const AdminBlogPage: FC = () => (
+import { IBlog } from "@/interfaces/blogs";
+import { GetStaticProps } from "next";
+import SingleBlogRow from "@/components/Blogs/SingleBlogRow";
+
+interface AdminBlogsPageProps {
+  blogs: IBlog[];
+}
+
+export const getStaticProps: GetStaticProps<AdminBlogsPageProps> = async () => {
+  const blogRes = await axiosInstance.get("/api/blogs");
+  return {
+    props: {
+      blogs: blogRes.data.body,
+    },
+  };
+};
+
+const AdminBlogPage: FC<AdminBlogsPageProps> = ({ blogs }) => (
   <>
-    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark m-6">
       <div className="py-6 px-4 md:px-6 xl:px-7.5">
         <h4 className="text-xl font-bold text-black dark:text-white">
-          Top Products
+          Бүх Мэдээ
         </h4>
       </div>
-      <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
-        <div className="col-span-3 flex items-center">
-          <p className="font-medium">Product Name</p>
+      <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark  md:px-6 2xl:px-7.5">
+        <div className="col-span-2 flex items-center">
+          <p className="font-medium">Гарчиг</p>
         </div>
-        <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium">Category</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Price</p>
+
+        <div className="col-span-1 hidden items-center sm:flex">
+          <p className="font-medium">Тайлбар</p>
         </div>
         <div className="col-span-1 flex items-center">
-          <p className="font-medium">Sold</p>
+          <p className="font-medium">Огноо</p>
         </div>
         <div className="col-span-1 flex items-center">
-          <p className="font-medium">Profit</p>
+          <p className="font-medium">Нийтлэгч</p>
+        </div>
+        <div className="col-span-1 flex items-center">
+          <p className="font-medium">Засах/Устгах</p>
         </div>
       </div>
-      <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
-        <div className="col-span-3 flex items-center">
-          <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-            <div className="h-12.5 w-15 rounded-md">
-              <Image width={120} height={100} src={placeholder} alt="Product" />
-            </div>
-            <p className="font-medium text-sm text-black dark:text-white">
-              Apple Watch Series 7
-            </p>
-          </div>
-        </div>
-        <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium text-sm text-black dark:text-white">
-            Electronics
-          </p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-black dark:text-white">$269</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-black dark:text-white">22</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-meta-3">$45</p>
-        </div>
-      </div>
-      {/* <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
-        <div className="col-span-3 flex items-center">
-          <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-            <div className="h-12.5 w-15 rounded-md">
-              <Image width={120} height={100} src={placeholder} alt="Product" />
-            </div>
-            <p className="font-medium text-sm text-black dark:text-white">
-              Apple Watch Series 7
-            </p>
-          </div>
-        </div>
-        <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium text-sm text-black dark:text-white">
-            Electronics
-          </p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-black dark:text-white">$269</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-black dark:text-white">22</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-meta-3">$45</p>
-        </div>
-      </div>
-      <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
-        <div className="col-span-3 flex items-center">
-          <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-            <div className="h-12.5 w-15 rounded-md">
-              <Image width={120} height={100} src={placeholder} alt="Product" />
-            </div>
-            <p className="font-medium text-sm text-black dark:text-white">
-              Macbook Pro M1
-            </p>
-          </div>
-        </div>
-        <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium text-sm text-black dark:text-white">
-            Electronics
-          </p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-black dark:text-white">$546</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-black dark:text-white">34</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-meta-3">$125</p>
-        </div>
-      </div>
-      <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
-        <div className="col-span-3 flex items-center">
-          <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-            <div className="h-12.5 w-15 rounded-md">
-              <Image width={120} height={100} src={placeholder} alt="Product" />
-            </div>
-            <p className="font-medium text-sm text-black dark:text-white">
-              Dell Inspiron 15
-            </p>
-          </div>
-        </div>
-        <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium text-sm text-black dark:text-white">
-            Electronics
-          </p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-black dark:text-white">$443</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-black dark:text-white">64</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-meta-3">$247</p>
-        </div>
-      </div>
-      <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
-        <div className="col-span-3 flex items-center">
-          <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-            <div className="h-12.5 w-15 rounded-md">
-              <Image width={120} height={100} src={placeholder} alt="Product" />
-            </div>
-            <p className="font-medium text-sm text-black dark:text-white">
-              HP Probook 450
-            </p>
-          </div>
-        </div>
-        <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium text-sm text-black dark:text-white">
-            Electronics
-          </p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-black dark:text-white">$499</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-black dark:text-white">72</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium text-sm text-meta-3">$103</p>
-        </div>
-      </div> */}
+      {blogs.map((blog) => (
+        <SingleBlogRow blog={blog} key={blog._id} />
+      ))}
     </div>
   </>
 );
