31ffcafede7a7cdc4a11a046bcac8922608d4e57
EnRose <enrosek95@gmail.com>
2023-05-22 17:03:00 +0800

    Done

--- client/src/components/Instructors/Dashboard/Reviews/ReviewAnswerCard.tsx
+++ client/src/components/Instructors/Dashboard/Reviews/ReviewAnswerCard.tsx
@@ -1,6 +1,6 @@
 import { ICourseReviewAnswer } from "@/interfaces/courses";
 import Image from "next/image";
-import { FC, useState } from "react";
+import { FC } from "react";
 import { HiOutlinePencilAlt, HiOutlineTrash } from "react-icons/hi";
 
 interface ReviewAnswerCardProps {
@@ -8,43 +8,39 @@ interface ReviewAnswerCardProps {
   avatar?: string;
 }
 
-const ReviewAnswerCard: FC<ReviewAnswerCardProps> = ({ answer, avatar }) => {
-  const [isEditing, setIsEditing] = useState<boolean>(false);
-
-  return (
-    <div className="py-3 flex items-center justify-between border-b border-b-border-1">
-      <div className="flex items-center gap-3">
-        {avatar && (
-          <div className="w-14 h-14 rounded-full overflow-hidden">
-            <Image
-              src={avatar}
-              alt=""
-              width={96}
-              height={96}
-              className="w-full h-full aspect-square object-cover"
-            />
-          </div>
-        )}
-
-        <div>
-          <span className="block mb-2 text-icon text-sm-regular">
-            {new Date(answer.updatedAt).toLocaleDateString()}
-          </span>
-          <p className="text-head text-base-regular leading-none">{answer.text}</p>
+const ReviewAnswerCard: FC<ReviewAnswerCardProps> = ({ answer, avatar }) => (
+  <div className="py-3 flex items-center justify-between border-b border-b-border-1">
+    <div className="flex items-center gap-3">
+      {avatar && (
+        <div className="w-14 h-14 rounded-full overflow-hidden">
+          <Image
+            src={avatar}
+            alt=""
+            width={96}
+            height={96}
+            className="w-full h-full aspect-square object-cover"
+          />
         </div>
-      </div>
+      )}
 
-      <div className="flex items-center gap-5 text-icon">
-        <button className="hover:text-color-1 duration-300">
-          <HiOutlinePencilAlt size={20} />
-        </button>
-
-        <button className="hover:text-color-1 duration-300">
-          <HiOutlineTrash size={20} />
-        </button>
+      <div>
+        <span className="block mb-2 text-icon text-sm-regular">
+          {new Date(answer.updatedAt).toLocaleDateString()}
+        </span>
+        <p className="text-head text-base-regular leading-none">{answer.text}</p>
       </div>
     </div>
-  );
-};
+
+    <div className="flex items-center gap-5 text-icon">
+      <button className="hover:text-color-1 duration-300">
+        <HiOutlinePencilAlt size={20} />
+      </button>
+
+      <button className="hover:text-color-1 duration-300">
+        <HiOutlineTrash size={20} />
+      </button>
+    </div>
+  </div>
+);
 
 export default ReviewAnswerCard;
